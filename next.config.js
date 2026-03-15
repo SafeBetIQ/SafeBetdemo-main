0f0ddb197ae980a9764e11083ec68937d49dbd22
SafeBetIQ <admin@satrimtechnologies.co.za>
2026-03-15 17:59:26 +0200

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    optimizePackageImports: [],
-  },
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
